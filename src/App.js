1b4579d4feea5c767e8c9a8ea766a49e00e41148
Danielscg2012@gmail.com <Danielscg2012@gmail.com>
2022-10-27 13:30:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+function App() {
+  const [num , setNum] = useState(0)
+  
+  return (
+    <div>
+      <h1 >{num}</h1>
+      <button onClick={(() =>  setNum(num +1) ) } >+</button>
+     <button onClick={(() =>  setNum(num -1) ) } >-</button>
+    </div>
+  );
+}
+
+export default App;
